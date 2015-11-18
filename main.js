27b8053e275331c0e150c8001b79f28b0f2ef702
chrisloomis13 <christopherfrenchloomis@gmail.com>
2015-11-18 12:04:19 -0800

    dzc weather tool for campaign

---
+++ main.js
@@ -0,0 +1,49 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var owm = require('./owm.js');
+var request = require('request');
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3000);
+app.use(express.static('public'));
+
+app.get('/',function(req,res){
+	var context = {};
+	request('http://api.openweathermap.org/data/2.5forecast/daily?q=corvallis&units=imperial&cnt=7&APPID=' + owm.key, get7Day);
+
+	function get7Day(err, response, body){
+		if(!err && response.statusCode < 400){
+			var params = [];
+			//var day = 1;
+			
+			for(q in body.list){
+				params.push({'daynum':(q+1),'temp':body.list[q].temp.day,'hum':body.list[q].humidity,'des':body.list[q].weather.description,'icon':body.list[q].weather.icon})
+			}
+			
+			res.render('7Day');
+		}
+		else{
+			console.log(err);
+			console.log(response.statusCode);
+		}
+	}
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
---
+++ omw.js
@@ -0,0 +1,3 @@
+module.exports = {
+	key: 'ac8c02ef8cf7667fb84c8c8dc20ec383'
+}
